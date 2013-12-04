f1b4bf2913ac89173161b95b3a351271345638cc
Michael de Silva <michael@mwdesilva.com>
2013-12-04 14:22:50 +0530

    Remove serialize call for removed column

--- app/models/catscan/scan.rb
+++ app/models/catscan/scan.rb
@@ -2,7 +2,6 @@ module Catscan
   class Scan < ActiveRecord::Base
     self.table_name = "catscan_scans"
 
-    serialize :payload
     attr_accessible :klass, :entity, :comment, :category,
       :error_message,
       :error_class,
